54f20bbec83cbc9b1b3e452fdf4c7fb891e5ddec
Brian Fields <brian@dmwdesign.net>
2018-10-24 22:10:14 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Brian Fields on 10/24/18.
+//  Copyright © 2018 Pantheon Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
